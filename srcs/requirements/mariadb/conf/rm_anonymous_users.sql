4b61fa0a0f7b04c218965ce6edfd3f8c2cc99678
Yusuf Salih Mamur <yusufsalihmamur@gmail.com>
2024-07-13 14:32:20 +0300

    final 1

--- srcs/requirements/mariadb/conf/rm_anonymous_users.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM mysql.user WHERE User='';
-UPDATE mysql.user SET Host='localhost' WHERE User='root'
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-FLUSH PRIVILEGES;
--- srcs/requirements/mariadb/tools/init-edit.sh
+++ srcs/requirements/mariadb/tools/init-edit.sh
@@ -3,14 +3,14 @@
 if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
     service mariadb start
 
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-    mysql -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 	sleep 3
-	mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
-	mysql stop
+    mysql -uroot -p${DB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+    mysql -uroot -p${DB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mysql -uroot -p${DB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mysql -uroot -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+	mariadb-admin -u root -p${DB_ROOT_PASSWORD} shutdown
+	service mariadb stop
 fi
 
 exec "$@"
