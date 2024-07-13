6a9974dc1028e3d00be45875783bc2b5396fe110
Yusuf Salih Mamur <yusufsalihmamur@gmail.com>
2024-07-13 12:23:47 +0300

    save

--- srcs/requirements/mariadb/conf/rm_anonymous_users.sql
+++ srcs/requirements/mariadb/conf/rm_anonymous_users.sql
@@ -1,6 +1,7 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_password';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'DB_ROOT_PASSWORD_HERE';
 DELETE FROM mysql.user WHERE User='';
-UPDATE mysql.user SET Host='localhost' WHERE User='root'
+UPDATE mysql.user SET Host='localhost' WHERE User='root';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+CREATE DATABASE IF NOT EXISTS `wordpress`;
 FLUSH PRIVILEGES;
--- srcs/requirements/mariadb/tools/init-edit.sh
+++ srcs/requirements/mariadb/tools/init-edit.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
-if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
+if [ ! -d "/var/lib/mariadb/${DB_NAME}" ]; then
+	echo "=> An empty or uninitialized MariaDB volume is detected in /var/lib/mysql"
     service mariadb start
 
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-    mysql -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
-    mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$DB_ROOT_PASSWORD');"
+	sed -i "s/DB_ROOT_PASSWORD_HERE/${DB_ROOT_PASSWORD}/g" /tmp/init-edit.sh
+    mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+    mariadb -e "CREATE USER IF NOT EXISTS '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_USERNAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mariadb -e "FLUSH PRIVILEGES;"
+    mariadb -e "SET PASSWORD FOR root@localhost = PASSWORD('$DB_ROOT_PASSWORD');"
 	sleep 3
-	mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
-	/etc/init.d/mysql stop
+	mariadb-admin -u root -p${DB_ROOT_PASSWORD} shutdown
+	mariadb stop
 fi
 
-exec mysqld_safe
+exec mariadbd-safe
